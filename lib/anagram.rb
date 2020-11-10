3a18cfe5fecfa6b28e0df95e95e1bc691ef9e087
Kimberly Baker <bakerk2010@gmail.com>
2020-11-10 16:58:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,8 @@ class Anagram
     @match = match
   end
   
-  diaper = Anagram.new("diaper")
-  diaper.match(%w(hello world zombies pants dipper))
-  
+  def
+    diaper = Anagram.new("diaper")
+    diaper.match(%w(hello world zombies pants dipper))
+  end
 end
