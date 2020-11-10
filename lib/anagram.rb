480be972fefae83a03f70782e3703945fdd946bf
Kimberly Baker <bakerk2010@gmail.com>
2020-11-10 16:58:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,11 @@
 class Anagram
   attr_accessor :word
   
-  def initialize
-    @match = match
+  def initialize(word)
+    @word = word
   end
   
-  def
+  def 
     diaper = Anagram.new("diaper")
     diaper.match(%w(hello world zombies pants dipper))
   end
